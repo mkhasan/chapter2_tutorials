87f5fc3360f57fd4ef8fffd78d7c9fb0541a474f
mkhasan <hasan@kaist.ac.kr>
2018-05-14 22:33:10 -0400

    Initial Commit

---
+++ src/example1_b.cpp
@@ -0,0 +1,34 @@
+/*
+ * example1_b.cpp
+ *
+ *  Created on: May 11, 2018
+ *      Author: usrc
+ */
+
+
+
+
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+
+#include "util.h"
+
+using namespace MY_LIB;
+
+void chatterCallback(const std_msgs::String::ConstPtr& msg) {
+	ROS_INFO("I heard : [%s]", msg->data.c_str());
+
+	TestPrint();
+
+}
+
+int main(int argc, char **argv) {
+	ros::init(argc, argv, "example1_b");
+	ros::NodeHandle n;
+	ros::Subscriber sub = n.subscribe("message", 1000, chatterCallback);
+	ros::spin();
+	return 0;
+
+}
+
+
